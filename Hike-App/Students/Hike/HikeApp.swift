f2d95e0de6dec42c285075d0673e447d4b17281a
HiHoi <ghtjd5087@gmail.com>
2023-11-01 23:25:34 +0900

    init

---
+++ Hike-App/Students/Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Hosung Lim on 10/22/23.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Hike-App/Students/Hike/Screen/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Hike
+//
+//  Created by Hosung Lim on 10/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        CardView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
